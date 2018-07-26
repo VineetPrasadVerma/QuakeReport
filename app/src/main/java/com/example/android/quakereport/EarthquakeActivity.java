1b180a0ba98d01395e885164f92d387a803ac635
Vineet Prasad Verma <vineetpd1996@gmail.com>
2018-07-26 15:08:24 +0530

    Add preference display.

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -63,7 +63,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
      * URL for earthquake data from the USGS dataset
      */
     private static final String USGS_REQUEST_URL =
-            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=5&limit=10";
+            "https://earthquake.usgs.gov/fdsnws/event/1/query";
 
     /**
      * Adapter for the list of earthquakes
